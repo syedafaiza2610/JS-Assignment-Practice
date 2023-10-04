9948d73bb31f28ce55690f22f2e4e1c7a0f87a12
syeda Faiza <syedafaiza2610@gmail.com>
2023-10-03 22:41:31 -0700

    Chapter 1 Done

---
+++ app.js
@@ -0,0 +1,18 @@
+// Chapter 1 
+// 1.	Write a script to greet your website visitor using JS alert box.
+ alert("Good Morning Welcome to My Page")
+
+ //2.	Write a script to display following message on your web page:
+ alert("Error! Please Enter a valid password.")
+
+ //3.	Write a script to display following message on your web page: (Hint : Use line break)
+ alert("Welcome to JS land...\n Happy Coding") 
+//  \n use for line break
+
+//4.	Write a script to display following messages in sequence:
+alert("Welcome to JS land...");
+alert("Happy Coding!")
+
+// 5.	Generate the following message through browser’s developer console:
+console.log("Hello I can run JS through web browser console");
+
