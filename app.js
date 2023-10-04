a5a97c80c42e5f0cce53bd739111afc16073bbc8
syeda Faiza <syedafaiza2610@gmail.com>
2023-10-03 23:14:54 -0700

    Chapter 2 Done

--- app.js
+++ app.js
@@ -1,18 +1,96 @@
 // Chapter 1 
 // 1.	Write a script to greet your website visitor using JS alert box.
- alert("Good Morning Welcome to My Page")
+//  alert("Good Morning Welcome to My Page")
 
  //2.	Write a script to display following message on your web page:
- alert("Error! Please Enter a valid password.")
+//  alert("Error! Please Enter a valid password.")
 
  //3.	Write a script to display following message on your web page: (Hint : Use line break)
- alert("Welcome to JS land...\n Happy Coding") 
+//  alert("Welcome to JS land...\n Happy Coding") 
 //  \n use for line break
 
 //4.	Write a script to display following messages in sequence:
-alert("Welcome to JS land...");
-alert("Happy Coding!")
+// alert("Welcome to JS land...");
+// alert("Happy Coding!")
 
 // 5.	Generate the following message through browser’s developer console:
-console.log("Hello I can run JS through web browser console");
+// console.log("Hello I can run JS through web browser console");
 
+
+//Chapter 2
+//1. Declare a variable called username.
+ var username;
+
+ //2. Declare a variable called myName & assign to it a string that represents your Full Name
+   var myName = "Faiza Parveen";
+   alert(myName)
+
+   //3. Write a script to 
+// a. declare a JS variable, titled message
+// b. assign “Hello World” to the variable message
+// c. display the message in alert box
+
+var message = "Hello World"
+alert(message)
+
+
+//4. Write a script to save student’s bio data in JS variables 
+// and show the data in alert boxes. Example:
+
+
+
+var myName = "Faiza Parveen";
+var age = 24;
+var uni = "University of Karachi";
+var course = "Mern Stack Developer";
+alert(myName)
+alert(age)
+alert(uni)
+alert(course)
+
+
+//5. Write a script to display the following alert using one JS variable:
+var a = " PIZZA \n PIZZ \n PIZ \n PI \n P"
+alert(a)
+
+//6. Fortune Teller: Why pay a fortuneteller when you can 
+// just program your fortune yourself?
+// Store the following into variables: number of children, 
+// partner’s name, geographic location, and job title
+
+var prof = "Mern Stack Developer"
+var country = " USA"
+var marry = "ABC"
+var child = "2 Kids"
+alert("You will be a "+ prof + "in" + " " + country + ", married with" +" " + marry + " " +"and have"+" " + child)
+
+
+// 7. Declare a variable called email & assign to it a string that 
+// represents your Email Address (e.g. abc@xyz.com). Show 
+// the below mentioned message in an alert box. (Hint: use 
+// string concatenation)
+ var email = "syedafaiza2610@gmail.com"
+ alert("My email address is" +" " + email)
+
+// 8. Declare a variable called book & give it the value “A 
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+
+var book = "A Smarter Way to learn JavaScript"
+alert("I am trying to learn from the book" + " " + book)
+ 
+// 9. Use the variables, declared in exercise 4 & show the details 
+// giving meaning to data. Like “My name is Ali Khan”, “I am 
+// 17 years old”, “I study in SSUET”, “I take Mobile & Cloud 
+// Computing, Module A classes”
+
+var myName = "Faiza Parveen";
+var age = 24;
+var uni = "University of Karachi";
+var course = "Mern Stack Developer";
+alert("My name is" + " " + myName + " " + "I am" + " " + age + " " + "years old" + " " + "I Study in" + " "+ uni + " " + "I am a" +" "+ course )
+ 
+
+
+//10. Write a script to display this in browser through JS
+ document.write("Yeah! I can Write HTML content through JS")
