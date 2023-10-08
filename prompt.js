395ea4f376c61390992f6221fa0957093a9d3b48
syeda Faiza <syedafaiza2610@gmail.com>
2023-10-07 22:37:06 -0700

    conditions practice

---
+++ prompt.js
@@ -0,0 +1,142 @@
+//1. Write a program that takes input a name from user & greet the user like this:
+// var userInput = prompt("Enter Your Name");
+// alert("Good Morning" + " "+ userInput);
+
+
+//2. Write a program to take input a number from user & display 
+// it’s multiplication table on your browser. If user does not enter
+// a new number, multiplication table of 5 should be displayed by 
+// default
+
+
+// var userInput = +prompt("Enter Your Number");
+// for (let i=1; i<=10; i++) {
+//     document.write(userInput+ " " + "x" + " "+ i + " "+ "="+" "+ userInput*i ,"<br>")   
+// }
+
+//3. Write a program to take “city” name as input from user. If user 
+// enters “Karachi”, welcome the user like this: “Welcome to city 
+// // of lights
+//  var city = prompt("Enter Your City");
+// //  var city1 = "karachi";
+// //  if(city === city1){
+// //     alert("Welcome to City of Lights")
+// //  }
+// //  else{
+// //     alert("Not a city")
+// // }
+//   var gender = prompt("Enter Your Gender");
+//   gender = gender.toLowerCase();
+//   if(gender == "male"){
+//     alert("Good Morning Sir")
+//   }
+//   else if(gender == "female"){
+//     alert("Good Morning Maam")
+//   }
+//   else{
+    
+//   }
+//   //5. Write a program to take input color of road traffic signal from 
+// the user & show the message according to this table:
+
+// var color = prompt("Enter Your color");
+// color = color.toLowerCase();
+// if(color == "red"){
+//   alert("Vehicle must stop")
+// }
+// else if(color == "yellow"){
+//   alert("Vehicles should get ready to move")
+// }
+// else if(color == "green"){
+//     alert("Vehicles can move now")
+//   }
+// else{ document.write("Wait")
+  
+// }
+//6. Write a program to take input max age & current age from 
+// user. If the current age is less than or equal to max age, show
+// the message “You are welcome”
+
+
+// var age = +prompt("Enter your current age");
+// var maxAge = 40;
+// if(age <= 40){
+//     alert("You are welcome")
+// }
+// else{alert("we are sorry")}
+
+
+//7. Write a program to take input remaining fuel in car (in litres) 
+// from user. If the current fuel is less than 0.25litres, show the 
+// message “Please refill the fuel in your car
+
+
+// var feul = +prompt("Enter your current feul");
+// var maxFeul = 0.25;
+// if(feul <= maxFeul){
+//     alert("Please refill the fuel in your car")
+// }
+// else{alert("you can go!")}
+
+
+// var a = 4;
+// if (++a === 5){
+//  alert("given condition for variable a is true");
+// }
+
+
+//  var b = 82;
+// if (b++ === 83){
+//  alert("given condition for variable b is true");
+// }
+
+//  var c = 12;
+// if (c++ === 13){
+//  alert("condition 1 is true");
+// }
+// if (c === 13){
+//  alert("condition 2 is true");
+// }
+// if (++c < 14){
+//  alert("condition 3 is true");
+// }
+// if(c === 14){
+//  alert("condition 4 is true");
+// }
+
+//  var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+//  alert("The cost equals");
+// }
+// if (true){
+//  alert("True");
+// }
+// if (false){
+//  alert("False");
+// }
+//  if("car" < "cat"){
+//  alert("car is smaller than cat");
+// }
+
+// 9. Write a program to take input the marks obtained in three 
+// subjects & total marks. Compute & show the resulting 
+// percentage on your page. Take percentage & compute grade as 
+// per following table: 
+// Percentage % Grade Remarks
+// Greater than or equal to 80 A-one Excellent
+// Greater than or equal to 70 A Good
+// Greater than or equal to 60 B You need to improve
+// Less than 60 Fail Sorry
+// Show the total marks, marks obtained, percentage, grade & 
+// remarks like
+
+
+
+
+
+
+
+
+
