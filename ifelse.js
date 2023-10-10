e324c7040b5cd35796ccf97ddaff6951acaef754
syeda Faiza <syedafaiza2610@gmail.com>
2023-10-09 23:12:01 -0700

    ifelse

---
+++ ifelse.js
@@ -0,0 +1,47 @@
+// . Write a program to check whether the given input number is 
+// divisible by 3 or else show a message “Number is not divisible 
+// by 3”.
+
+
+// var num1 = +prompt("Enter your number");
+// if (num1 %3 === 0 ) {
+//     alert("Number is divisible by 3")
+// } 
+// else{
+//     alert("not divisible by 3")
+// }
+
+// . Write a program that checks whether the given input is an even 
+// number or an odd number
+// var num = +prompt("Enter any number");
+// if (num % 2 == 0) {
+//     alert("Its an even number")
+// } else {
+//     alert("Its an odd number")
+// }
+
+// 3. Write an if/else statement with the following condition:
+// If the variable age is greater than 18, output "Old enough", 
+// otherwise output "Too young".
+
+// var age = +prompt("Enter your age");
+// if (age >= 18) {
+//     alert("Old Enough :)")
+// } else {
+//     alert("Too Young!")
+// }
+
+
+// Write a program that prompts the user for their name, and then 
+// displays a special greeting to that person if their name is the 
+// same as yours. If the name entered by the user is anything 
+// other than your name, your code should not produce any 
+// output.
+
+
+var name1 = prompt("Enter your name");
+if (name1 === "faiza") {
+    alert("Good Morning")
+} else {
+    
+}
