8f224e566c909d0d7dc2ac4732ac9850993f1f73
syeda Faiza <syedafaiza2610@gmail.com>
2023-10-09 22:16:47 -0700

    practice complete

--- prompt.js
+++ prompt.js
@@ -186,55 +186,41 @@
 // offer them 10% discount & show the receipt in your browser
 
 
-var item1Name = prompt("Enter the name of item1");
-var item2Name = prompt("Enter the name of item2");
-var item1Quantity = +prompt("Enter the quantity of item1");
-var item2Quantity = +prompt("Enter the quantity of item2");
-var item1price = 1000;
-var item2price = 700;
-var shippingCharges = 250;
-var totalCostBeforeDiscount = (item1price * item1Quantity + item2price * item2Quantity) + shippingCharges;
-var discount = 0.1 * totalCostBeforeDiscount;
-    var totalCostAfterDiscount = totalCostBeforeDiscount - discount;
-// var discount = total*10/100;
 
-if (totalCostBeforeDiscount > 2000) {
+// var item1Name = prompt("Enter the name of item1");
+// var item2Name = prompt("Enter the name of item2");
+// var item1Quantity = +prompt("Enter the quantity of item1");
+// var item2Quantity = +prompt("Enter the quantity of item2");
+// var item1price = 1000;
+// var item2price = 700;
+// var shippingCharges = 250;
+// var totalCostBeforeDiscount = (item1price * item1Quantity + item2price * item2Quantity) + shippingCharges;
+// var discount = 0.1 * totalCostBeforeDiscount;
+//     var totalCostAfterDiscount = totalCostBeforeDiscount - discount;
+// // var discount = total*10/100;
+
+// if (totalCostBeforeDiscount > 2000) {
     
-    document.write("<h1>" + "A Shopping Cart" + "</h1>" + "<br>" +
-"Price of item 1 " + " " + item1Name + " " + "is:" + " "+ item1price +"<br>" +
-"Quantity of item 1 " + " " + item1Name + " " + "is:" + " "+ item1Quantity+ "<br>" +
-"Price of item 2 " + " " + item2Name + " " + "is:" +" "+ item2price +"<br>" +
-"Quantity of item 2 " + " " + item2Name + " " + "is:" + " "+item2Quantity + "<br>" +
-"Shipping Charges " + " " + " " + "is:" +" "+ shippingCharges + "<br>" +"<br>"+
-"Total cost of your order " + " " + "is:" + " "+totalCostBeforeDiscount+ "<br>" +
-"Discounted cost of your order " + " " + "is:"+ totalCostAfterDiscount+ "<br>" 
-)
-}
-else{
-    document.write("<h1>" + "A Shopping Cart" + "</h1>" + "<br>" +
-    "Price of item 1 " + " " + item1Name + " " + "is:" + " "+ item1price +"<br>" +
-    "Quantity of item 1 " + " " + item1Name + " " + "is:" + " "+ item1Quantity+ "<br>" +
-    "Price of item 2 " + " " + item2Name + " " + "is:" +" "+ item2price +"<br>" +
-    "Quantity of item 2 " + " " + item2Name + " " + "is:" + " "+item2Quantity + "<br>" +
-    "Shipping Charges " + " " + " " + "is:" +" "+ shippingCharges + "<br>" +"<br>"+
-    "Total cost of your order " + " " + "is:" + " "+totalCostBeforeDiscount+ "<br>" 
-    )
-}
-
-
-
-// } else {
-//     total;
-// }
-// document.write("<h1>" + "A Shopping Cart" + "</h1>" + "<br>" +
-// "Price of item 1 :" + " " + item_name1 + " " + "is" + price1 +"<br>" +
-// "Quantity of item 1 :" + " " + item_name1 + " " + "is" + quantityitem_1 + "<br>" +
-// "Price of item 2 :" + " " + item_name2 + " " + "is" + price2 +"<br>" +
-// "Quantity of item 2 :" + " " + item_name2 + " " + "is" + quantityitem_2 + "<br>" +
-// "Shipping Charges :" + " " + " " + "is" + delivery_charges + "<br>" +"<br>"+
-// "Total cost of your order :" + " " + "is" + total+ "<br>" 
+//     document.write("<h1>" + "A Shopping Cart" + "</h1>" + "<br>" +
+// "Price of item 1 " + " " + item1Name + " " + "is:" + " "+ item1price +"<br>" +
+// "Quantity of item 1 " + " " + item1Name + " " + "is:" + " "+ item1Quantity+ "<br>" +
+// "Price of item 2 " + " " + item2Name + " " + "is:" +" "+ item2price +"<br>" +
+// "Quantity of item 2 " + " " + item2Name + " " + "is:" + " "+item2Quantity + "<br>" +
+// "Shipping Charges " + " " + " " + "is:" +" "+ shippingCharges + "<br>" +"<br>"+
+// "Total cost of your order " + " " + "is:" + " "+totalCostBeforeDiscount+ "<br>" +
+// "Discounted cost of your order " + " " + "is:"+ totalCostAfterDiscount+ "<br>" 
 // )
-
+// }
+// else{
+//     document.write("<h1>" + "A Shopping Cart" + "</h1>" + "<br>" +
+//     "Price of item 1 " + " " + item1Name + " " + "is:" + " "+ item1price +"<br>" +
+//     "Quantity of item 1 " + " " + item1Name + " " + "is:" + " "+ item1Quantity+ "<br>" +
+//     "Price of item 2 " + " " + item2Name + " " + "is:" +" "+ item2price +"<br>" +
+//     "Quantity of item 2 " + " " + item2Name + " " + "is:" + " "+item2Quantity + "<br>" +
+//     "Shipping Charges " + " " + " " + "is:" +" "+ shippingCharges + "<br>" +"<br>"+
+//     "Total cost of your order " + " " + "is:" + " "+totalCostBeforeDiscount+ "<br>" 
+//     )
+// }
 // 11. Guess game:
 // Store a secret number (ranging from 1 to 10) in a variable. 
 // Prompt user to guess the secret number.
@@ -269,6 +255,220 @@ else{
 // else{
 //     alert("not divisible by 3")
 // }
+
+// 13. Names & Total scores of two teams are taken as input. Write 
+// a program that shows which team has won the game or show if 
+// there is a tie. (Team A or Team B)
+
+
+// var team1 = prompt("Enter team 1 name");
+// var score1 = +prompt("Enter your team total score");
+// var team2 = prompt("Enter team 2 name");
+// var score2 = +prompt("Enter your team total score");
+
+// if (score1 <= 300 || score1 != score2) {
+//     alert("Team" + " " + team1+" "+ "is winner")
+// } 
+// else if(score1 >= 300 || score1 != score2){
+//     alert("Team" + " " + team2+" "+ "is winner")
+// }
+// else if (score1 === score2)
+// alert("There is a tie")
+// else {
+//     alert();
+// }
+
+
+// 14. Take a string, a number and a Boolean in three variables. 
+// Write a program that checks the type of variables & responds 
+// accordingly. Like :
+
+// var str = prompt();
+// var num = +prompt();
+// var bool = prompt();
+
+// alert(str + " " + "is" + " " + typeof str);
+// alert(num + " " + "is" + " " + typeof num);
+// alert(bool + " " + "is" + " " + typeof bool);
+
+
+
+// 15. Write a program that checks whether the given input is an 
+// even number or an odd number
+
+
+// var num = +prompt("Enter any number");
+// if (num % 2 == 0) {
+//     alert("Its an even number")
+// } else {
+//     alert("Its an odd number")
+// }
+
+// 16. Weather in Karachi nowadays is too cool, write a program 
+// that takes temperature as input and shows a message based on 
+// following criteria
+// a. T > 40 then “It is too hot outside.”
+// b. T > 30 then “The Weather today is Normal.”
+// c. T > 20 then “Today’s Weather is cool.”
+// d. T > 10 then “OMG! Today’s weather is so Cool.”
+
+// var temp = +prompt("Enter temperature");
+// if (temp >= 40) {
+//     alert("It is too hot outside.")
+
+// } else if (temp >= 30){
+//     alert("The Weather today is Normal.")
+// }
+// else if (temp >= 20){
+//     alert("Today’s Weather is cool.")
+// }
+// else if (temp >= 10){
+//     alert("OMG! Today’s weather is so Cool.")
+// }
+// else{
+
+// }
+// 18. Write a program that takes user input day name. If the day is 
+// Monday, Tuesday, Wednesday, Thursday or Friday, then show 
+// “It’s a week day”. If the day is Saturday then show “It’s 
+// weekend”. If the day is Sunday then show “Yay! It’s a holiday”
+
+// var day = prompt("Enter day");
+// if (day === "monday","tuesday","wednesday","thursday","friday") {
+//     alert("It’s a week day")
+// } else if(day === "saturday"){
+//     alert("It’s a weekend")
+// }
+// else if(day === "sunday"){
+//     alert("Yay! It’s a holiday")
+// }
+
+
+// 19. Write a program that takes input user’s score, if it’s greater 
+// than 50, say “You are passed”. Otherwise, show “Try again!”
+
+// var num = +prompt("enter number");
+// if (num >= 50) {
+//     alert("you are passed")
+// } else {
+//     alert ("try again")
+// }
+
+
+
+// Write a program that:
+// a. takes 2 numbers.
+// b. tells whichever number is the greater (higher) number.
+// c. tells if they are equal
+// Show the output to make sure it works (e.g. "The greater 
+// number of 5 and 10 is 10.").
+
+// var num1 = +prompt("enter num 1");
+// var num2 = +prompt("enter num 2");
+// if (num1 > num2){
+//     alert("The greater number of" + " " + num1 +" "+"and"+" "+ num2 + " "+"is" + " "+ num1)
+    
+// } 
+//  else if(num1 < num2)
+//  {
+//     alert("The greater number of" + " " + num1 +" "+"and"+" "+ num2 + " "+"is" + " "+ num2)
+// }
+// else if(num1 === num2)
+// {
+//     alert("Both numbers are equal")
+// }
+// else{
+
+// }
+
+// 21. The Translator:
+// Write a program that:
+// a. takes 1 input, a language code (e.g. "es", "de", "en")
+// b. tells "Hello, World" for the given language, for atleast 3 
+// languages. It should default to returning English.
+// (Hint: use translate.google.com to check the translation of 
+// hello world in different languages
+// var code = prompt("Enter your Language code");
+// if (code === "en") {
+//     alert("Hello World")
+// } else if(code === "es")
+//  {
+//     alert("Hola Mundo")
+// }
+// else if(code === "de"){
+//     alert("Hallo Welt")
+// }
+// else{
+//     alert("Hello World")
+// }
+//22. Write a program to take input a number & tell whether it’s a positive or negative number.
+
+// var num =+prompt("enter your number");
+// if (num > 0) {
+//     alert("Number is Positive")
+// } else if(num < 0) {
+//     alert("Number is Negative")
+// }
+// else{
+
+// }
+
+// The Pluralizer: Write a program that:
+// a. takes 2 inputs, a noun and a number.
+// b. tells the number and pluralized form, like "5 cats" or "1 
+// dog".
+// Run the program for a few different inputs and show the result 
+// to make sure it works
+
+// var num =+prompt("enter your number");
+// var noun = prompt("Enter Noun");
+// if (num ===1) {
+//     alert(num + noun)
+// } else {
+//     alert(num +" "+  noun + "s")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
