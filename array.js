0d3cad4f1923b7b78ed691f8c6faf44158f4d015
syeda Faiza <syedafaiza2610@gmail.com>
2023-10-10 22:57:28 -0700

    array practice

---
+++ array.js
@@ -0,0 +1,98 @@
+// Declare an empty array using JS literal notation to store 
+// student names in future.
+
+// var studentsName = [];
+// studentsName.push();
+// studentsName.push();
+// studentsName.push();
+// studentsName.push();
+
+// . Declare an empty array using JS object notation to store 
+// student names in future.
+
+// var studentsName = {};
+
+
+// 3. Declare and initialize a strings array.
+// var name1;
+// name1 = ["Faiza","Saba", "Hira" , "Uzma"];
+
+// 4. Declare and initialize a numbers array.
+// var num;
+// num = [1,2,3,4,5,6,7];
+
+// 5. Declare and initialize a boolean array.
+
+// var bool;
+// bool = ["true","false"];
+
+// 6. Declare and initialize a mixed array
+// var mixed;
+// mixed = ["faiza","Saba",24 , 26,"false"];
+// alert(mixed)
+
+// 7. Declare and Initialize an array and store available mobile 
+// networks in Pakistan.
+// var networks;
+// networks = ["Ufone","Jazz","Zong","Telenor"];
+
+// 8. Declare and Initialize an array and store available education 
+// qualifications in Pakistan (e.g. SSC, HSC, BCS, BS, BCOM, MS, 
+// M. Phil., PhD). Show the listed qualifications in your browser 
+// like:
+
+// var qualifications = ["SSC","HSC","BCS","BS","BCOM","MS","M.Phil","PhD"];
+// for (var i = 0; i < qualifications.length; i++) {
+//   document.write((i+1) + ") " + qualifications[i] ,"<br>");
+// }
+
+
+// 9. Declare and initialize an empty array to store top movies of 
+// 2015. Add movies one by one in an array. Display the elements 
+// & length of array in your browser. (Use array’s length method)
+
+// var movies = [];
+// movies.push("Age of Altron");
+// movies.push("Tiger");
+// movies.push("Jawan");
+// movies.push("Spider");
+// movies.push("Inside Out");
+// for (var i = 0; i < movies.length; i++) {
+//   document.write((i+1) + ") " + movies[i] ,"<br>"); 
+// }
+//   document.write("The Length of an array is " + " "+ movies.length);
+
+// 10. Declare and Initialize an array with your favorite cars. Show 
+// a. First index of the array
+// b. Car at first index of the array
+// c. Last index of the array
+// ARRAYS |	JAVASCRIPT
+// Page	3 of	8
+// d. Car at last index of the array
+
+// var favoriteCars = ["Audi","Mehran","Farari","Lamborghini"];
+// var firstIndex = 0;
+// var carAtFirstIndex = favoriteCars[firstIndex];
+// var lastIndex = favoriteCars.length - 1;
+// var carAtLastIndex = favoriteCars[lastIndex];
+// document.write("a. First index of the array: " + firstIndex); 
+// document.write("b. Car at first index of the array: " + carAtFirstIndex);
+// document.write("c. Last index of the array: " + lastIndex);
+// document.write("d. Car at last index of the array: " + carAtLastIndex);
+
+// . Write a program to store 3 student names in an array. Take 
+// another array to store score of these three students. Assume 
+// that total marks are 500 for each student, display the scores & 
+// percentages of students like:
+// var studentNames = ["John", "Alice", "Bob"];
+// for (var i = 0; i < studentNames.length; i++) {
+//   var studentName = studentNames[i];
+//   var studentScore = studentScores[i];
+//   var percentage = (studentScore / 500) * 100;
+
+//   document.write("Student: " + studentName);
+//   document.write("Score: " + studentScore);
+//   document.write("Percentage: " + percentage.toFixed(2) + "%");
+//   document.write();
+// }
+
--- ifelse.js
+++ ifelse.js
@@ -39,9 +39,140 @@
 // output.
 
 
-var name1 = prompt("Enter your name");
-if (name1 === "faiza") {
-    alert("Good Morning")
-} else {
+// var name1 = prompt("Enter your name");
+// if (name1 === "faiza") {
+//     alert("Good Morning")
+// } else {
     
-}
+// }
+
+// 8. Write a program that takes time as input from user in 24 hours 
+// clock format like: 1900 = 7pm. Implement the following case 
+// using if, else & else if statements
+
+// var time = +prompt("Enter time in 12 hrs format");
+// if (time >= "0000" && time <= 1200) {
+//     alert("Good Morning!")
+// } 
+// else if (time >= 1200 && time <= 1700) {
+//     alert("Good afternoon!")
+// } 
+// else if (time >= 1700 && time <= 2100) {
+//     alert("Good Evening!")
+// } 
+// else if (time >= 2100 && time <= 2359) {
+//     alert("Good Night!")
+// } 
+// else {
+    
+// }
+
+// 11. Write a program that adds an else statement to the following 
+// script to display “You are not Fahad”
+// var firstName = "Ali";
+// if (firstName === "Fahad") {
+//  document.write("Hello Fahad!");
+// }
+
+// 11. Write a program that adds an else statement to the following 
+// script to display “You are not Fahad”
+
+// var firstName = "Ali";
+// if (firstName === "Fahad") {
+//  document.write("Hello Fahad!");
+// }
+// else{
+//     document.write("You are not Fahad")
+// }
+
+// 12. This if/else statement does not work. Try to fix it:
+//  var greeting;
+//  var hour = 13;
+//  if (hour < 18) {
+//  greeting = "Good day";
+//  else
+//  greeting = "Good evening";
+//  } 
+
+//  var greeting;
+//  var hour = 13;
+//  if (hour < 18) {
+//  alert (greeting = "Good day");
+//  }
+// else {
+//  alert(greeting = "Good evening");
+//  }
+// 13. Write a JavaScript program that accept two integers and 
+// display the larger. Also show if the two integers are equal.
+
+// var num1 = +prompt("enter num 1");
+// var num2 = +prompt("enter num 2");
+// if (num1 > num2){
+//     alert("The greater number of" + " " + num1 +" "+"and"+" "+ num2 + " "+"is" + " "+ num1)
+    
+// } 
+//  else if(num1 < num2)
+//  {
+//     alert("The greater number of" + " " + num1 +" "+"and"+" "+ num2 + " "+"is" + " "+ num2)
+// }
+// else if(num1 === num2)
+// {
+//     alert("Both numbers are equal")
+// }
+// else{
+
+// }
+
+// 14. Write a program that takes input a number from user & state 
+// whether the number is positive, negative or zero.
+
+// var num =+prompt("enter your number");
+// if (num > 0) {
+//     alert("Number is Positive")
+// } else if(num < 0) {
+//     alert("Number is Negative")
+// }
+// else{
+
+// }
+
+// 16. Write a program that stores value in a variable & tell whether 
+// the type of that variable is a "number", "string", "boolean" or 
+// “undefined
+
+// var str = "faiza";
+// var num = 3;
+// var bool = false;
+// var math ;
+
+// alert(str + " " + "is" + " " + typeof str);
+// alert(num + " " + "is" + " " + typeof num);
+// alert(bool + " " + "is" + " " + typeof bool);
+// alert(math + " " + "is" + " " + typeof math);
+
+
+
+// 17. Write a program that takes a character (i.e. string of length 1) 
+// and returns true if it is a vowel, false otherwise
+
+// var input = prompt("Enter your alphabet");
+// var ans = ("a","e","i","o","u");
+// if (input === ans) {
+//     alert("true")
+// } 
+// else {
+//     alert("false")
+    
+// }
+
+
+// 18. Choose the correct comparison operator to display "true", 
+// when:
+// 10 is NOT equal to 8.
+
+// if (10 != 8) {
+//     alert("true")
+// } else {
+//     alert("false")
+// }
+
